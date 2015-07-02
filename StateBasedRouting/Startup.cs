afb15dbc66820170831ec95798dea8217c29f2f7
Adam Friedman <adam.friedman@itaas.dimensiondata.com>
2015-07-03 09:14:01 +1000

    Add logging.

--- StateBasedRouting/Startup.cs
+++ StateBasedRouting/Startup.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
 
 namespace StateBasedRouting
 {
+	/// <summary>
+	///		Configuration for the state-based routing demo.
+	/// </summary>
     public class Startup
     {
         /// <summary>
@@ -30,13 +30,21 @@ namespace StateBasedRouting
 		/// <param name="app">
 		///		The application pipeline builder.
 		/// </param>
-        public void Configure(IApplicationBuilder app)
+		/// <param name="loggerFactory">
+		///		The system logger factory.
+		/// </param>
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
 	        if (app == null)
 		        throw new ArgumentNullException("app");
 
-	        app.UseStaticFiles();
+			if (loggerFactory == null)
+				throw new ArgumentNullException("loggerFactory");
+
+			app.UseStaticFiles();
 	        app.UseMvc();
-        }
+
+			loggerFactory.AddConsole();
+		}
     }
 }
